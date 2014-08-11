c3f5708f174154ff87b05398c74b0a8e656906b4
ghowles <ghowles@dn527td8.sunet>
2014-08-10 21:27:32 -0700

    Initial Commit

---
+++ firsttryTests/firsttryTests.m
@@ -0,0 +1,34 @@
+//
+//  firsttryTests.m
+//  firsttryTests
+//
+//  Created by ghowles on 8/10/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface firsttryTests : XCTestCase
+
+@end
+
+@implementation firsttryTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
